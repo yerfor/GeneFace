5cb1d8a3147229c42237d12b4168ec2da5ab6376
yerfor <zhenhuiye@zju.edu.cn>
2023-02-27 13:07:22 +0800

    update script

--- scripts/infer_postnet.sh
+++ scripts/infer_postnet.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
-export Video_ID=Zhang2
-export Wav_ID=how_i_want_to_say_goodbye
-export Postnet_Ckpt_Steps=12000 # please reach to `docs/train_models.md` to get some tips about how to select an approprate ckpt_steps!
+export Video_ID=May
+export Wav_ID=zozo
+export Postnet_Ckpt_Steps=6000 # please reach to `docs/train_models.md` to get some tips about how to select an approprate ckpt_steps!
 
 python inference/postnet/postnet_infer.py \
     --config=checkpoints/${Video_ID}/postnet/config.yaml \
