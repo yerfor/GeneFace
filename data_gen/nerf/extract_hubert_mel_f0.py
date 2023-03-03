10ea8a50560b4781e0a45cd91b3435793d548f8f
yerfor <zhenhuiye@zju.edu.cn>
2023-03-03 15:11:29 +0800

    update

--- data_gen/nerf/extract_hubert_mel_f0.py
+++ data_gen/nerf/extract_hubert_mel_f0.py
@@ -1,17 +1,18 @@
 import soundfile as sf
 import numpy as np
+import torch
 from argparse import ArgumentParser
 from data_gen.process_lrs3.process_audio_hubert import get_hubert_from_16k_speech
 from data_gen.process_lrs3.process_audio_mel_f0 import extract_mel_f0_from_fname
 
 parser = ArgumentParser()
-parser.add_argument('--video_id', type=str, default='Zhang2', help='')
+parser.add_argument('--video_id', type=str, default='May', help='')
 args = parser.parse_args()
 
 person_id = args.video_id
 wav_16k_name = f"data/processed/videos/{person_id}/aud.wav"
-hubert_npy_name = f"data/processed/videos/{person_id}/hubert.npy"
-mel_f0_npy_name = f"data/processed/videos/{person_id}/mel_f0.npy"
+hubert_npy_name = f"data/processed/videos/{person_id}/aud_hubert.npy"
+mel_f0_npy_name = f"data/processed/videos/{person_id}/aud_mel_f0.npy"
 speech_16k, _ = sf.read(wav_16k_name)
 hubert_hidden = get_hubert_from_16k_speech(speech_16k)
 np.save(hubert_npy_name, hubert_hidden.detach().numpy())
