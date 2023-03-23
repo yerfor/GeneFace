0d05060f52bbbb86e080f1bfab50310ac32085aa
yerfor <zhenhuiye@zju.edu.cn>
2023-03-23 10:52:28 +0800

    move to newer `librosa.filters.mel`

--- data_gen/process_lrs3/process_audio_mel_f0.py
+++ data_gen/process_lrs3/process_audio_mel_f0.py
@@ -44,7 +44,7 @@ def extract_mel_from_fname(wav_path,
     # get mel basis
     fmin = 0 if fmin == -1 else fmin
     fmax = sample_rate / 2 if fmax == -1 else fmax
-    mel_basis = librosa.filters.mel(sample_rate, fft_size, num_mels, fmin, fmax)
+    mel_basis = librosa.filters.mel(sr=sample_rate, n_fft=fft_size, n_mels=num_mels, fmin=fmin, fmax=fmax)
     mel = mel_basis @ spc
 
     mel = np.log10(np.maximum(eps, mel))  # (n_mel_bins, T)
